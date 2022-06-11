046aec5cf9da2021defc2ff10059ae4c12acd78b
Tarik02 <Taras.Fomin@gmail.com>
2022-06-11 22:03:37 +0300

    fix variables not added to global env

--- web/public/index.php
+++ web/public/index.php
@@ -7,6 +7,7 @@ use Symfony\Component\Dotenv\Dotenv;
 use Tarik02\PccontrolWeb\Controller;
 
 $dotenv = new Dotenv();
+$dotenv->usePutenv();
 $dotenv->load(__DIR__ . '/../.env');
 
 $request = Request::createFromGlobals();
